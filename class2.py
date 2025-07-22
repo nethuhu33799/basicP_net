862fd6f706bfb083e451e93c19c3da3a44b42287
Student <Student@Train11-5-PC133.control.ad.sit.kmutt.ac.th>
2025-07-22 15:26:30 +0700

    Add class2

---
+++ class2.py
@@ -0,0 +1,88 @@
+# # # age = 18
+# # # print(age)
+# # # type(age)
+
+# # # num = 5
+# # # print(type(num))
+
+# # # num2 = 1.1
+# # # print(type(num2))
+
+# # # boolean = True
+# # # print(type(boolean))
+
+# # # name = "net"
+# # # print(type(name),name)
+
+# # age = "18"
+# # print(type(age))
+# # # print(type(int(age)))
+
+# # age2 = int(age)
+# # print(type(age2))
+
+# # num = 5.5
+# # print(type(num))
+
+# # num2 = int(num)
+# # print(type(num2))
+# # print((num2))
+
+
+# # num2=float(num2)
+# # print(num2)
+
+# # Age = 199
+# # print(type(Age))
+
+# # Age = str(Age)
+# # print(Age)
+
+# # tft = "True"
+# # print(type(tft))
+
+# # tft2 = bool(tft)
+# # print(type(tft2))
+
+# # TFT = str(tft2)
+# # print(type(TFT))
+
+# # sum = 1+2.5
+# # print(sum)
+
+# # Sum = 1 + True
+# # print(Sum)
+
+
+
+# # n1 = "r"
+# # n2 = "r"
+
+# # print(n1 == n2)
+
+# # print(len("hghghghgh"))
+
+# # print("aa"<"aaaa")
+
+# AGe = input("Enter your age : ")
+# print ("Your age : ",AGe)
+
+# AGe2 = input(int("Enter your age : "))
+# print ("Your age : ",AGe2)
+
+# MyName = input("Enter Your Name : ")
+# print("My Name is      :",MyName)
+
+#--------------------------------------------------------------------------#
+# age = int(input("Enter your age : "))
+# if age > 18 :
+#     print("Your age more than 18")
+# else: print("Your age lower than 18")
+
+age = int(input("Enter your age : "))
+if age<18:
+    print ("your age more than 18.")
+elif age<20:
+    print ("your age more than 20.")
+else age<30:
+    print ("your age more than 30.")
