06807f7a1d190a9a140b24b48ffe0b96bedac3d8
nethuhu <tthn52762@gmail.com>
2025-07-23 09:24:50 +0700

    แก้ไข

--- class2_workshop.py
+++ class2_workshop.py
@@ -1,16 +1,16 @@
 distance = int(input("Enter your distance : "))
 
-if distance<0:
-    print ("Wtf !? ")
-elif distance<5:
-    print ("Not sending items")
-elif 5<distance<50:
-    print ("price = 10 Bath")
-elif 51<distance<100:
-    print ("price = 15 Bath")
-elif 101<distance<300:
-    print ("price = 25 Bath")
-elif distance<500:
-    print ("price = 35 Bath")
-else :
-    print ("price = 45 Bath")
+if distance < 0:
+    print("Wtf !?")
+elif distance < 5:
+    print("Not sending items")
+elif distance <= 50:
+    print("price = 10 Bath")
+elif distance <= 100:
+    print("price = 15 Bath")
+elif distance <= 300:
+    print("price = 25 Bath")
+elif distance <= 500:
+    print("price = 35 Bath")
+else:
+    print("price = 45 Bath")
