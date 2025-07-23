9409f288b67a0a75ce64fdd0610481fd68cc1104
Tanaphon Ratsamejaroensuk <tthn52762@gmail.com>
2025-07-23 19:43:09 +0700

    class3_work.py
    แก้บัค ver.2

--- class3/class3_work.py
+++ class3/class3_work.py
@@ -1,9 +1,13 @@
 while True :
     HP = 200
+    character = ["1.Nobi Nobita","2.Gon Furikusu","3.Kurapika"]
+    print("Character : ",character[0:3])
+    Chioice_character = int(input("Enter your Chioice : "))
+    print("Your Character is : ",character[Chioice_character-1])
+    
     chioice = int(input("พิมพ์ 1 เพื่อต่อสู้ , พิพม์ 2 เพื่อหนี!!! : "))
     if chioice==1:
         num = int(input("ต้องการสู้กี่รอบ : "))
-
         for i in range (num):
             print("----HP monster = ",HP,"----")
             if(HP==0):print("***********  ตาย  ***********")
@@ -14,18 +18,18 @@ while True :
             print("3.ดินสอ : atk 200")
             print("-----------------------------")
             item = int(input("เลือกอาวุทธ 1 ชิ้น ในรอบนี้ "))
-            print("-----------------------------")
             if item == 1:
                 HP-=100
             elif item == 2:
                 HP-=50
             elif item == 3:
                 HP-=200
-            elif HP < 0:
+            if HP < 0:
                 HP = 20
                 print("HP monster = ",HP)
             print("////////////////////////////////////////////////////")
         print("คุณตาย เนื่องจาก จัดการมอนไม่ได้")
         print("เริ่มเกมใหม่ เริ่มเกมใหม่ เริ่มเกมใหม่")
+        print(".....................................................")
             
             
